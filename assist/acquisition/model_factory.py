ea918dcfef022855802d08aec9b17eb7df209994
Pu Wang <60357737+wangpuup@users.noreply.github.com>
2023-02-15 03:28:35 +0100

    Update model_factory.py

--- assist/acquisition/model_factory.py
+++ assist/acquisition/model_factory.py
@@ -1,9 +1,5 @@
 '''@file model_factory.py
 contains the model factory'''
-#import sys
-#sys.path.append(os.getcwd())
-#sys.path.append('/esat/spchdisk/scratch/pwang/pre-train-FSC/capsule')
-#from assist.acquisition import tfmodel
 
 def factory(name):
     '''model factory method
@@ -17,18 +13,9 @@ def factory(name):
     if name == 'rccn':
         import tfmodel.rccn
         return tfmodel.rccn.RCCN
-    elif name == 'rccn_spk':
-        import tfmodel.rccn_spk
-        return tfmodel.rccn_spk.RCCN_SPK
-    elif name == 'pccn':
-        import tfmodel.pccn
-        return tfmodel.pccn.PCCN
     elif name == 'encoder_decoder':
         import tfmodel.encoder_decoder
         return tfmodel.encoder_decoder.EncoderDecoder
-    elif name == 'nmf':
-        import nmf
-        return nmf.NMF
 
     else:
         raise Exception('unknown acquisition type %s' % name)
